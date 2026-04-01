a90a783f543d63b9a427ec2564861df6b7f64374
sk1218180-cell <sk1218180@gmail.com>
2026-04-01 14:05:58 +0530

    Utils code respond and discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config()
 
 
 connectDB()
-
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : 
+            ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection FAILED !!! ", err);
+})
 
 
 
