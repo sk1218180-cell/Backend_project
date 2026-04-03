b7833b99d1c032bc1d2862047c16f19741f8cf5f
sk1218180-cell <sk1218180@gmail.com>
2026-04-03 12:38:01 +0530

    Add Cloudinary integration and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,64 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
+        api_key: 'process.env.CLOUDINARY_API_KEY', 
+        api_secret: 'process.env.CLOUDINARY_API_SECRET' // Click 'View API Keys' above to copy your API secret
+    });
+
+
+      const uploadOnCloudinary = async (localFilePath) => {
+        try {
+            if (!localFilePath) return null
+            // Upload the image to Cloudinary
+            const response = await cloudinary.uploader.upload
+            (localFilePath, {
+                resource_type: "auto"
+            })
+            // file has been uploaded successfully
+            console.log("File is uploaded on cloudinary", 
+                response.url);
+                return response;
+            
+        } catch (error) {
+            fs.unlinkSync(localFilePath) // remove the file from local storage
+            return null
+        }
+      }
+
+
+      export { uploadOnCloudinary }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const uploadResult = await cloudinary.uploader
+    //    .upload(
+    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+    //            public_id: 'shoes',
+    //        }
+    //    )
+    //    .catch((error) => {
+    //        console.log(error);
+    //    });
+    
+    // console.log(uploadResult);
